c245f1c0395b2801ecd33cd4331ede5191b3b82e
Andini Lukman <andinilukman0820@gmail.com>
2026-04-30 12:24:29 +0800

    feat: pembuatan migration, seeder, dan factory untuk Buku

---
+++ database/factories/BukuFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Buku;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Buku>
+ */
+class BukuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/seeders/BukuSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Buku;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BukuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Buku::create([
+            'judul' => 'Laskar Pelangi',
+            'penulis' => 'Andrea Hirata',
+            'penerbit' => 'Gramedia',
+            'tahun_terbit' => 2005,
+            'genre' => 'Novel'
+        ]);
+    }
+}
