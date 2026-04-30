ad3e052da8965532de5d55375f2ab9d63c77bad3
Andini Lukman <andinilukman0820@gmail.com>
2026-04-30 11:49:22 +0800

    feat: penyiapan blade component untuk layout utama

---
+++ resources/views/Layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Data Buku</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-dark bg-dark mb-4">
+        <div class="container">
+            <a href="/" class="navbar-brand">Data Buku</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+</body>
+
+</html>
